e2481ef98001b37bfa4b8d5736986307d56e76b6
Shahad842 <shahad_9215@hotmail.com>
2024-10-06 10:37:51 +0300

    Initial Commit

---
+++ khafot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  khafot
+//
+//  Created by Shahad Alhothali on 03/04/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ khafot/khafotApp.swift
@@ -0,0 +1,17 @@
+//
+//  khafotApp.swift
+//  khafot
+//
+//  Created by Shahad Alhothali on 03/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct khafotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
